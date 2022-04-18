6a53aedd73d47e9c5b0b8c9aaa68c6bfcfd2f14d
soraJang <skysora85@gmail.com>
2022-04-18 11:23:42 +0900

    fix : axios-interceptor 중복 등록 오류 수정
    - axios interceptor이 이미 등록되어있는 경우, 추가등록하지 않도록 수정함.

--- source/nuxtend/components/common/axios-interceptor.vue
+++ source/nuxtend/components/common/axios-interceptor.vue
@@ -23,8 +23,21 @@ export default {
   created() {
     Vue.use(LoadingOverlay);
     let api = Vue.prototype.$api;
-    api.interceptors.request.use(this.requestSuccess, this.requestError);
-    api.interceptors.response.use(this.responseSuccess, this.responseError);
+    /**
+     * 각각의 root 페이지에서 axios-interceptor를 호출하게 되어있음.
+     * 페이지가 변경될때마다 axios가 생성되어서, interceptor가 중복으로 등록됨.
+     *
+     * 수정방안 1 :
+     *    index.vue를 root로 처리하고 axios-interceptor를 등록하려 했으나, 추후에 root 페이지별로 분리할 가능성이 있어서 보류함.
+     * 수정방안 2 :
+     *    interceptor를 store로 관리하려 했으나, 처리가 복잡하여 보류함.
+     *
+     * 이미 등록된 request가 없는 경우에만 interceptor을 등록해줌.
+     */
+    if (api.interceptors.request.handlers.length <= 1) {
+      api.interceptors.request.use(this.requestSuccess, this.requestError);
+      api.interceptors.response.use(this.responseSuccess, this.responseError);
+    }
   },
   methods: {
     getErrorMessage(message) {
