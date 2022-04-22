8175c35f62e86095ba0e4de6860a5c1ef5f44f17
ygkim97 <dnjdn21@naver.com>
2022-04-22 14:24:02 +0900

    feat : basic logo, menu 컴포넌트 추가 (AIPLATFORM-188)

---
+++ source/nuxtend/components/basic/basic-logo.vue
@@ -0,0 +1,63 @@
+<template lang="html">
+  <div>
+    <router-link :to="url" :is="routerLinkUse">
+      <img :width="width" :height="height" :src="src" :alt="alt" />
+      <span class="text-title" v-if="textTitle">{{ textTitle }}</span>
+    </router-link>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: "biasic-logo",
+  extends: {},
+  props: {
+    url: {
+      type: String,
+      require: false
+    },
+    width: {
+      type: String,
+      require: false
+    },
+    height: {
+      type: String,
+      require: false
+    },
+    src: {
+      type: String,
+      require: false
+    },
+    alt: {
+      type: String,
+      require: false
+    },
+    textTitle: {
+      type: String,
+      require: false
+    }
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    routerLinkUse() {
+      return this.url === undefined || this.url === "" ? "div" : "router-link";
+    }
+  },
+  components: {},
+  watch: {},
+  methods: {}
+};
+</script>
+
+<style lang="scss">
+.text-title {
+  font-size: 2rem;
+  vertical-align: top;
+  color: black;
+}
+a:link {
+  text-decoration: none;
+}
+</style>
---
+++ source/nuxtend/components/basic/basic-menu.vue
@@ -0,0 +1,61 @@
+<template lang="html">
+  <div>
+    <ul>
+      <li v-for="(data, i) in menuList" :key="'menu_' + i">
+        <router-link :to="menuList[i]['url']">{{
+          menuList[i]["menuName"]
+        }}</router-link>
+        <span v-if="separatorUse(menuList.length, i)">{{ separator }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: "basic-menu",
+  extends: {},
+  props: {
+    menuList: {
+      type: Array,
+      require: true
+    },
+    separator: {
+      type: String,
+      require: false
+    }
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  components: {},
+  watch: {},
+  methods: {
+    separatorUse(menuListLength, index) {
+      let notEmpSeparator = false;
+      notEmpSeparator = this.separator !== undefined && this.separator !== "";
+
+      return notEmpSeparator && menuListLength !== index + 1;
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+ul {
+  list-style: none;
+  display: flex;
+  li {
+    a {
+      text-decoration: none;
+      padding: 0px 10px;
+      color: black;
+    }
+    a:hover {
+      text-decoration: underline;
+      color: red;
+    }
+  }
+}
+</style>
