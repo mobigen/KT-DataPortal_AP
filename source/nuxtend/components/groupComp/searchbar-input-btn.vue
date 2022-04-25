deb96410ff02ad7a7170be3e71ae88f2ceb9b131
soraJang <skysora85@gmail.com>
2022-04-25 10:38:34 +0900

    feat : app 페이지 구성 및 통합검색 페이지 component 분리 외 (AIPLATFORM-192)
    - layout 분리
    - app 페이지 상위메뉴 페이지 구현
    - 통합검색 component 분리 외

---
+++ source/nuxtend/components/groupComp/searchbar-input-btn.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+  <div>
+    <h5>ex) search bar와 search 버튼이 함께 되어있는 component</h5>
+    <input type="text" />
+
+    <!-- text button or icon , or button component -->
+    <button value="search"></button>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: "searchbar-input-btn",
+  extends: {},
+  props: {
+    bar: {},
+    foo: {},
+    fooBar: {}
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  components: {},
+  watch: {},
+  methods: {},
+  created() {}
+};
+</script>
+
+<style lang="scss">
+// @import ""
+</style>
---
+++ source/nuxtend/pages/app/my.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <div>
+    <h5>마이페이지</h5>
+    <nuxt-child />
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: "app-my",
+  extends: {},
+  props: {
+    bar: {},
+    foo: {},
+    fooBar: {}
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  components: {},
+  watch: {},
+  methods: {},
+  created() {}
+};
+</script>
+
+<style lang="scss">
+// @import ""
+</style>
