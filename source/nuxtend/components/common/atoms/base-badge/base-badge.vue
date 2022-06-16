fc2e2315038f01d1815f23feae878632df817efe
soraJang <skysora85@gmail.com>
2022-06-16 15:00:40 +0900

    feat : components/ 파일 추가

---
+++ source/nuxtend/components/common/atoms/base-badge/base-badge.vue
@@ -0,0 +1,17 @@
+<template>
+  <!--  최소 단위인 Badge Element Component-->
+  <div class="badge">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseBadge",
+  props: {},
+  computed: {},
+  methods: {}
+};
+</script>
+
+<style></style>
