e797a9dc77df1d7b49cab5807b1e5b6dd7f33ffa
soraJang <skysora85@gmail.com>
2022-06-24 13:16:17 +0900

    fix : 요청데이터 목록 화면 구현
    - /portal/ui/users/requested list 구현

---
+++ source/nuxtend/pages/portal/ui/users/requested/index.vue
@@ -0,0 +1,56 @@
+<template lang="html">
+  <div>
+    <h5>데이터 활용 신청내역</h5>
+
+    <basic-table
+      componentId=""
+      :headerList="requestedData.header"
+      :dataList="requestedData.body"
+      :headerLocale="$t('header')"
+      :bodyLocale="$t('body')"
+      :hideColumns="['id']"
+      rowKey="id"
+      :useSerialNum="false"
+      serialNumText=""
+      :useTableButton="false"
+      :tableButtonText="{}"
+      @buttonAction=""
+      @columnAction=""
+      :keyActionText="{}"
+      @keyAction=""
+    />
+  </div>
+</template>
+
+<i18n src="./index.json"></i18n>
+
+<script type="text/javascript">
+import basicTable from "@component/aiPlatform/basic/basic-table";
+import { mapActions, mapGetters } from "vuex";
+
+export default {
+  name: "users-requested",
+  extends: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters("users/requested/requested", ["requestedData"])
+  },
+  components: {
+    basicTable
+  },
+  watch: {},
+  methods: {
+    ...mapActions("users/requested/requested", ["getRequestedData"])
+  },
+  created() {
+    this.getRequestedData();
+  }
+};
+</script>
+
+<style lang="scss">
+// @import ""
+</style>
