ec126cd0ff197b8161fa28dc0f962496c8b96a1d
ygkim97 <dnjdn21@naver.com>
2022-07-11 13:47:40 +0900

    feat: 활용 신청내역 페이지네이션 적용

--- source/nuxtend/store/users/requested/requested.js
+++ source/nuxtend/store/users/requested/requested.js
@@ -19,30 +19,26 @@ export const actions = {
     // TODO : 요청데이터 목록 조회시, 조회 filter 처리 안됨
     console.log(requestParams.searchParam);
 
-    /*
-     TODO: pagination 처리
-     const paging =
+    const paging =
       rootGetters["module/pagination/paging"][requestParams.paginationKey];
-    const paramAPI = `?perPage=${paging.itemsPerPage}&curPage=${paging.page}`;*/
+    const paramAPI = `?perPage=${paging.itemsPerPage}&curPage=${paging.page}`;
 
     this.$axios
       .get(
-        this.$config.ROUTE_API_META_PREFIX +
-          "/getUseBoardDataList" +
-          "?perPage=100&curPage=1"
+        this.$config.ROUTE_API_META_PREFIX + "/getUseBoardDataList" + paramAPI
       )
       .then((d) => {
         commit("setRequestedData", d);
 
         // setTotalPage
-        /*dispatch(
+        dispatch(
           "module/pagination/setTotalCount",
           {
             key: requestParams.paginationKey,
             totalCount: d.totalcount
           },
           { root: true }
-        );*/
+        );
       });
 
     // console.log(requestParam);
