ce4a66af48be1e274bb6cfab66508e0b6404285e
ygkim97 <dnjdn21@naver.com>
2022-07-11 09:37:35 +0900

    feat: header 메뉴 url 설정

--- source/nuxtend/layouts/kt/header/header.vue
+++ source/nuxtend/layouts/kt/header/header.vue
@@ -8,12 +8,20 @@
         <div class="gnb-area">
           <div class="top-bar">
             <ul class="top-menu">
-              <li v-if="user === null" class="top-menu__item"><NuxtLink to="/sample/login" class="top-menu__link" href="#">로그인</NuxtLink></li>
-              <li v-if="user === null" class="top-menu__item"><a class="top-menu__link" href="#">회원가입</a></li>
+              <li v-if="user === null" class="top-menu__item">
+                <NuxtLink to="/sample/login" class="top-menu__link" href="#"
+                  >로그인</NuxtLink
+                >
+              </li>
+              <li v-if="user === null" class="top-menu__item">
+                <a class="top-menu__link" href="#">회원가입</a>
+              </li>
               <li v-if="user !== null" class="top-menu__item user-name">
                 <a class="top-menu__link" href="#">{{ user.name }}</a>
               </li>
-              <li v-if="user !== null" class="top-menu__item"><a class="top-menu__link" href="/logout">로그아웃</a></li>
+              <li v-if="user !== null" class="top-menu__item">
+                <a class="top-menu__link" href="/logout">로그아웃</a>
+              </li>
               <li class="top-menu__item">
                 <a class="top-menu__link" href="/kt/my-wish">
                   관심데이터
@@ -41,7 +49,11 @@
               <span class="hidden">메뉴열기</span></base-button>
           </div> 2022-06-16 DEL : 모바일없음 -->
 
-          <nav class="nav" :class="isMobileGnbOpen ? 'nav--open' : ''" @mouseleave="isLnbOpen = false">
+          <nav
+            class="nav"
+            :class="isMobileGnbOpen ? 'nav--open' : ''"
+            @mouseleave="isLnbOpen = false"
+          >
             <!--<div class="mobile-top-bar">
               <div class="mobile-top-bar__home-button">
                 <base-button class="button--link button--icon">
@@ -77,93 +89,149 @@
             </div> 2022-06-16 DEL : 모바일없음 -->
             <ul class="gnb">
               <li class="gnb__item" @mouseover="isLnbOpen = true">
-                <NuxtLink to="/kt/search" class="gnb__link"><span>데이터</span></NuxtLink>
+                <NuxtLink
+                  to="/portal/ui/meta/search/fullSearch"
+                  class="gnb__link"
+                  ><span>데이터</span></NuxtLink
+                >
                 <ul class="lnb">
                   <li class="lnb__item">
-                    <NuxtLink to="/kt/search" class="lnb__link">데이터검색</NuxtLink>
+                    <NuxtLink
+                      to="/portal/ui/meta/search/fullSearch"
+                      class="lnb__link"
+                      >데이터검색</NuxtLink
+                    >
                   </li>
                   <li class="lnb__item">
-                    <NuxtLink to="/kt/search-map" class="lnb__link">시각화검색</NuxtLink>
+                    <NuxtLink to="/kt/search-map" class="lnb__link"
+                      >시각화검색</NuxtLink
+                    >
                   </li>
                   <li class="lnb__item">
-                    <NuxtLink to="/kt/registration" class="lnb__link">데이터 신청</NuxtLink>
+                    <NuxtLink to="/kt/registration" class="lnb__link"
+                      >데이터 신청</NuxtLink
+                    >
                   </li>
                 </ul>
               </li>
               <li class="gnb__item" @mouseover="isLnbOpen = true">
-                <NuxtLink to="/kt/usage" class="gnb__link"><span>데이터 활용</span></NuxtLink>
+                <NuxtLink to="/kt/usage" class="gnb__link"
+                  ><span>데이터 활용</span></NuxtLink
+                >
                 <ul class="lnb">
                   <li class="lnb__item">
-                    <NuxtLink to="/kt/example" class="lnb__link">데이터 활용사례</NuxtLink>
+                    <NuxtLink to="/kt/example" class="lnb__link"
+                      >데이터 활용사례</NuxtLink
+                    >
                   </li>
                   <li class="lnb__item">
-                    <NuxtLink to="/kt/campaign" class="lnb__link">AB캠페인</NuxtLink>
+                    <NuxtLink to="/kt/campaign" class="lnb__link"
+                      >AB캠페인</NuxtLink
+                    >
                   </li>
                   <li class="lnb__item">
-                    <NuxtLink to="/kt/analysis" class="lnb__link">데이터 분석 툴</NuxtLink>
+                    <NuxtLink to="/kt/analysis" class="lnb__link"
+                      >데이터 분석 툴</NuxtLink
+                    >
                   </li>
                   <li class="lnb__item">
-                    <NuxtLink to="/kt/project" class="lnb__link">프로젝트</NuxtLink>
+                    <NuxtLink to="/kt/project" class="lnb__link"
+                      >프로젝트</NuxtLink
+                    >
                   </li>
                   <li class="lnb__item">
-                    <NuxtLink to="/kt/content-register" class="lnb__link">데이터콘텐츠 등록</NuxtLink>
+                    <NuxtLink to="/kt/content-register" class="lnb__link"
+                      >데이터콘텐츠 등록</NuxtLink
+                    >
                   </li>
                 </ul>
               </li>
               <li class="gnb__item" @mouseover="isLnbOpen = true">
-                <NuxtLink to="/kt/letsdata" class="gnb__link"><span>커뮤니티</span></NuxtLink>
+                <NuxtLink to="/kt/letsdata" class="gnb__link"
+                  ><span>커뮤니티</span></NuxtLink
+                >
                 <ul class="lnb">
                   <li class="lnb__item">
-                    <NuxtLink to="/kt/letsdata" class="lnb__link">Let’s DATA</NuxtLink>
+                    <NuxtLink to="/kt/letsdata" class="lnb__link"
+                      >Let’s DATA</NuxtLink
+                    >
                   </li>
                   <li class="lnb__item">
-                    <NuxtLink to="/kt/docean" class="lnb__link">D Ocean 협의체</NuxtLink>
+                    <NuxtLink to="/kt/docean" class="lnb__link"
+                      >D Ocean 협의체</NuxtLink
+                    >
                   </li>
                 </ul>
               </li>
               <li class="gnb__item" @mouseover="isLnbOpen = true">
-                <NuxtLink to="/kt/notice" class="gnb__link"><span>이용안내</span></NuxtLink>
+                <NuxtLink to="/kt/notice" class="gnb__link"
+                  ><span>이용안내</span></NuxtLink
+                >
                 <ul class="lnb">
                   <li class="lnb__item">
-                    <NuxtLink to="/kt/notice" class="lnb__link">공지사항</NuxtLink>
+                    <NuxtLink to="/kt/notice" class="lnb__link"
+                      >공지사항</NuxtLink
+                    >
                   </li>
                   <li class="lnb__item">
                     <NuxtLink to="/kt/faq" class="lnb__link">FAQ</NuxtLink>
                   </li>
                   <li class="lnb__item">
-                    <NuxtLink to="/kt/inquiry" class="lnb__link">문의하기</NuxtLink>
+                    <NuxtLink to="/kt/inquiry" class="lnb__link"
+                      >문의하기</NuxtLink
+                    >
                   </li>
                   <li class="lnb__item">
-                    <NuxtLink to="/kt/service" class="lnb__link">서비스 소개</NuxtLink>
+                    <NuxtLink to="/kt/service" class="lnb__link"
+                      >서비스 소개</NuxtLink
+                    >
                   </li>
                   <li class="lnb__item">
-                    <NuxtLink to="/kt/reference" class="lnb__link">자료실</NuxtLink>
+                    <NuxtLink to="/kt/reference" class="lnb__link"
+                      >자료실</NuxtLink
+                    >
                   </li>
                 </ul>
               </li>
               <li class="gnb__item" @mouseover="isLnbOpen = true">
-                <NuxtLink to="/kt/search" class="gnb__link"><span>마이페이지</span></NuxtLink>
+                <NuxtLink to="/kt/search" class="gnb__link"
+                  ><span>마이페이지</span></NuxtLink
+                >
                 <ul class="lnb">
                   <li class="lnb__item">
-                    <NuxtLink to="/kt/my-dashboard" class="lnb__link">대시보드</NuxtLink>
+                    <NuxtLink to="/kt/my-dashboard" class="lnb__link"
+                      >대시보드</NuxtLink
+                    >
                   </li>
                   <li class="lnb__item">
-                    <NuxtLink to="/kt/my-wish" class="lnb__link">관심데이터</NuxtLink>
+                    <NuxtLink to="/kt/my-wish" class="lnb__link"
+                      >관심데이터</NuxtLink
+                    >
                   </li>
                   <li class="lnb__item">
-                    <NuxtLink to="/kt/my-usage" class="lnb__link">내 활용내역</NuxtLink>
+                    <NuxtLink to="/portal/ui/users/my-usage" class="lnb__link"
+                      >내 활용내역</NuxtLink
+                    >
                   </li>
                   <li class="lnb__item">
-                    <NuxtLink to="/kt/my-example" class="lnb__link">내 활용사례</NuxtLink>
+                    <NuxtLink to="/kt/my-example" class="lnb__link"
+                      >내 활용사례</NuxtLink
+                    >
                   </li>
                   <li class="lnb__item">
-                    <NuxtLink to="/kt/my-data" class="lnb__link">데이터 신청현황</NuxtLink>
+                    <NuxtLink to="/kt/my-data" class="lnb__link"
+                      >데이터 신청현황</NuxtLink
+                    >
                   </li>
                   <li class="lnb__item">
-                    <NuxtLink to="/kt/my-inquiry" class="lnb__link">문의내역</NuxtLink>
+                    <NuxtLink to="/kt/my-inquiry" class="lnb__link"
+                      >문의내역</NuxtLink
+                    >
                   </li>
                   <li class="lnb__item">
-                    <NuxtLink to="/kt/my-project" class="lnb__link">프로젝트 관리</NuxtLink>
+                    <NuxtLink to="/kt/my-project" class="lnb__link"
+                      >프로젝트 관리</NuxtLink
+                    >
                   </li>
                 </ul>
               </li>
